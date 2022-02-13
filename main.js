4ceab71c674826a62de1782d0551b6c89520af9e
fabio98p <parisifabio98@gmail.com>
2022-02-13 01:47:34 +0100

    sistemato il movimento

--- main.js
+++ main.js
@@ -1,49 +1,85 @@
-var direction = "left"
-var a = 0
-var position_x = 0
-var position_y = 0
-let griglia = document.getElementById("griglia")
-let div = document.createElement("li")
-for (let x = 0; x < 12 ; x++) {
-    for (let y = 0; y < 12; y++) {
-        griglia.innerHTML += `<li id="c${x}_r${y}" class="box box-x${x} box-y${y}"></li>`
-    }
-}
-setInterval(() => {
-    switch (direction) {
-        case "left":
-            movment("x", false)
-            break;
-        case "top":
-            movment("y", true)
-            break;
-        case "right":
-            movment("x", true)
-            break;
-        case "bottom":
-            movment("y", false)
-            break;
-        default:
-            break;
-    }
-}, 1000);
-function movment(direction, axes){
-    //console.log("dir",(direction == "x")?position_x:position_y);
-    //console.log("axes",((axes)? 0: 11));
-    //console.log(((direction == "x")?position_x:position_y) == ((axes)? 0: 11));
-    if (((direction == "x")?position_x:position_y) == ((axes)? 0: 11)) {
-        
-    } else {
-        //console.log((axes)?a+1:a-1);
-        
-        ((direction == "x")?position_x:position_y) = ((axes)?(((direction == "x")?position_x:position_y)+1):(((direction == "x")?position_x:position_y)-1)) 
-        console.log(a, position_y, position_x);
-        console.log(getposition());
+var direction = "left";
+var a = 0;
+var position_x = 0;
+var position_y = 0;
+let griglia = document.getElementById("griglia");
+let div = document.createElement("li");
+CreationGame();
 
-    }
+setInterval(() => {
+	Update();
+}, 1000);
+function MovmentLeft() {
+	if (position_x == 11) {
+		getposition().classList.remove("active");
+		position_x = 0;
+		getposition().classList.add("active");
+	} else {
+		getposition().classList.remove("active");
+		position_x++;
+		getposition().classList.add("active");
+	}
 }
-function getposition(){
-    return document.getElementById(`c${position_x}_r${position_y}`)
+function MovmentRight() {
+	if (position_x == 0) {
+		getposition().classList.remove("active");
+		position_x = 11;
+		getposition().classList.add("active");
+	} else {
+		getposition().classList.remove("active");
+		position_x++;
+		getposition().classList.add("active");
+	}
+}
+function MovmentUp() {
+	if (position_y == 0) {
+		getposition().classList.remove("active");
+		position_y = 11;
+		getposition().classList.add("active");
+	} else {
+		getposition().classList.remove("active");
+		position_y++;
+		getposition().classList.add("active");
+	}
+}
+function MovmentDown() {
+	if (position_y == 11) {
+		getposition().classList.remove("active");
+		position_y = 0;
+		getposition().classList.add("active");
+	} else {
+		getposition().classList.remove("active");
+		position_y++;
+		getposition().classList.add("active");
+	}
+}
+function CreationGame() {
+	for (let x = 0; x < 12; x++) {
+		for (let y = 0; y < 12; y++) {
+			griglia.innerHTML += `<li id="c${x}_r${y}" class="box box-x${x} box-y${y}"></li>`;
+		}
+	}
+}
+function Update() {
+	switch (direction) {
+		case "left":
+			MovmentLeft(direction);
+			break;
+		case "top":
+			MovmentUp(direction);
+			break;
+		case "right":
+			MovmentRight(direction);
+			break;
+		case "bottom":
+			MovmentDown(direction);
+			break;
+		default:
+			break;
+	}
+}
+function getposition() {
+	return document.getElementById(`c${position_x}_r${position_y}`);
 }
 // if (axes == ((direction)? 0: 11)) {
 //     getposition().classList.remove("active")
@@ -52,6 +88,6 @@ function getposition(){
 // } else {
 //     getposition().classList.remove("active")
 //     console.log("else");
-//     position_x++  
+//     position_x++
 //     getposition().classList.add("active")
 // }
