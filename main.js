ff1e227d486cbf5b77e212e24b216d6e3941c7ec
fabio98p <parisifabio98@gmail.com>
2022-02-13 23:37:23 +0100

    creato movimento all'interno della griglia di gioco

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-var direction = "left";
-var a = 0;
+//sito keydown (https://keycode.info/)
+var direction = "down";
 var position_x = 0;
 var position_y = 0;
 let griglia = document.getElementById("griglia");
 let div = document.createElement("li");
+
+function CreationGame() {
+	for (let y = 0; y < 12; y++) {
+		for (let x = 0; x < 12; x++) {
+			griglia.innerHTML += `<li id="x${x}_y${y}" class="box box-x${x} box-y${y}"></li>`;
+		}
+	}
+}
 CreationGame();
 
 setInterval(() => {
+	Comand();
 	Update();
-}, 1000);
+}, 500);
+function Comand() {
+console.log(position_x, position_y, direction)
+}
 function MovmentLeft() {
-	if (position_x == 11) {
+	if (position_x == 0) {
 		getposition().classList.remove("active");
-		position_x = 0;
+		position_x = 11;
 		getposition().classList.add("active");
 	} else {
 		getposition().classList.remove("active");
-		position_x++;
+		position_x--;
 		getposition().classList.add("active");
 	}
 }
 function MovmentRight() {
-	if (position_x == 0) {
+	if (position_x == 11) {
 		getposition().classList.remove("active");
-		position_x = 11;
+		position_x = 0;
 		getposition().classList.add("active");
 	} else {
 		getposition().classList.remove("active");
@@ -38,7 +50,7 @@ function MovmentUp() {
 		getposition().classList.add("active");
 	} else {
 		getposition().classList.remove("active");
-		position_y++;
+		position_y--;
 		getposition().classList.add("active");
 	}
 }
@@ -53,25 +65,18 @@ function MovmentDown() {
 		getposition().classList.add("active");
 	}
 }
-function CreationGame() {
-	for (let x = 0; x < 12; x++) {
-		for (let y = 0; y < 12; y++) {
-			griglia.innerHTML += `<li id="c${x}_r${y}" class="box box-x${x} box-y${y}"></li>`;
-		}
-	}
-}
 function Update() {
 	switch (direction) {
 		case "left":
 			MovmentLeft(direction);
 			break;
-		case "top":
+		case "up":
 			MovmentUp(direction);
 			break;
 		case "right":
 			MovmentRight(direction);
 			break;
-		case "bottom":
+		case "down":
 			MovmentDown(direction);
 			break;
 		default:
@@ -79,15 +84,24 @@ function Update() {
 	}
 }
 function getposition() {
-	return document.getElementById(`c${position_x}_r${position_y}`);
+	return document.getElementById(`x${position_x}_y${position_y}`);
 }
-// if (axes == ((direction)? 0: 11)) {
-//     getposition().classList.remove("active")
-//     position_x = (axes)? 0: 11
-//     getposition().classList.add("active")
-// } else {
-//     getposition().classList.remove("active")
-//     console.log("else");
-//     position_x++
-//     getposition().classList.add("active")
-// }
+document.addEventListener("keydown", (event) => {
+	console.log("keydown");
+	if (event.isComposing || event.keyCode === 87) {
+		direction = "up";
+		return;
+	}
+	if (event.isComposing || event.keyCode === 68) {
+		direction = "right";
+		return;
+	}
+	if (event.isComposing || event.keyCode === 83) {
+		direction = "down";
+		return;
+	}
+	if (event.isComposing || event.keyCode === 65) {
+		direction = "left";
+		return;
+	}
+});
