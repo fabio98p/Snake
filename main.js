7af422a327011100496258afa3ad2f2d83957979
fabio98p <parisifabio98@gmail.com>
2022-03-01 23:03:45 +0100

    creato classe snake che comanda il gioco

--- main.js
+++ main.js
@@ -1,107 +1,113 @@
 //sito keydown (https://keycode.info/)
-var direction = "down";
-var position_x = 0;
-var position_y = 0;
-let griglia = document.getElementById("griglia");
-let div = document.createElement("li");
-
-function CreationGame() {
-	for (let y = 0; y < 12; y++) {
-		for (let x = 0; x < 12; x++) {
-			griglia.innerHTML += `<li id="x${x}_y${y}" class="box box-x${x} box-y${y}"></li>`;
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+class snake {
+	direction = "left";
+	position_x = 4;
+	position_y = 4;
+	griglia = document.getElementById("griglia");
+	div = document.createElement("li");
+	constructor() {
+		this.CreationGame();
+		this.StartGame();
+	}
+	CreationGame() {
+		for (let y = 0; y < 12; y++) {
+			for (let x = 0; x < 12; x++) {
+				this.griglia.innerHTML += `<li id="x${x}_y${y}" class="box box-x${x} box-y${y}"></li>`;
+			}
 		}
 	}
-}
-CreationGame();
+	StartGame() {
+		setInterval(() => {
 
-setInterval(() => {
-	Comand();
-	Update();
-}, 500);
-function Comand() {
-console.log(position_x, position_y, direction)
-}
-function MovmentLeft() {
-	if (position_x == 0) {
-		getposition().classList.remove("active");
-		position_x = 11;
-		getposition().classList.add("active");
-	} else {
-		getposition().classList.remove("active");
-		position_x--;
-		getposition().classList.add("active");
+			this.Update();
+		}, 500);
+	}
+	Changedirection(direction){
+		this.direction = direction
+	}
+	MovmentLeft() {
+		if (this.position_x == 0) {
+			this.getposition().classList.remove("active");
+			this.position_x = 11;
+			this.getposition().classList.add("active");
+		} else {
+			this.getposition().classList.remove("active");
+			this.position_x--;
+			this.getposition().classList.add("active");
+		}
+	}
+	MovmentRight() {
+		if (this.position_x == 11) {
+			this.getposition().classList.remove("active");
+			this.position_x = 0;
+			this.getposition().classList.add("active");
+		} else {
+			this.getposition().classList.remove("active");
+			this.position_x++;
+			this.getposition().classList.add("active");
+		}
+	}
+	MovmentUp() {
+		if (this.position_y == 0) {
+			this.getposition().classList.remove("active");
+			this.position_y = 11;
+			this.getposition().classList.add("active");
+		} else {
+			this.getposition().classList.remove("active");
+			this.position_y--;
+			this.getposition().classList.add("active");
+		}
+	}
+	MovmentDown() {
+		if (this.position_y == 11) {
+			this.getposition().classList.remove("active");
+			this.position_y = 0;
+			this.getposition().classList.add("active");
+		} else {
+			this.getposition().classList.remove("active");
+			this.position_y++;
+			this.getposition().classList.add("active");
+		}
+	}
+	Update() {
+		switch (this.direction) {
+			case "left":
+				this.MovmentLeft(this.direction);
+				break;
+			case "up":
+				this.MovmentUp(this.direction);
+				break;
+			case "right":
+				this.MovmentRight(this.direction);
+				break;
+			case "down":
+				this.MovmentDown(this.direction);
+				break;
+			default:
+				break;
+		}
+	}
+	getposition() {
+		return document.getElementById(`x${this.position_x}_y${this.position_y}`);
 	}
 }
-function MovmentRight() {
-	if (position_x == 11) {
-		getposition().classList.remove("active");
-		position_x = 0;
-		getposition().classList.add("active");
-	} else {
-		getposition().classList.remove("active");
-		position_x++;
-		getposition().classList.add("active");
-	}
-}
-function MovmentUp() {
-	if (position_y == 0) {
-		getposition().classList.remove("active");
-		position_y = 11;
-		getposition().classList.add("active");
-	} else {
-		getposition().classList.remove("active");
-		position_y--;
-		getposition().classList.add("active");
-	}
-}
-function MovmentDown() {
-	if (position_y == 11) {
-		getposition().classList.remove("active");
-		position_y = 0;
-		getposition().classList.add("active");
-	} else {
-		getposition().classList.remove("active");
-		position_y++;
-		getposition().classList.add("active");
-	}
-}
-function Update() {
-	switch (direction) {
-		case "left":
-			MovmentLeft(direction);
-			break;
-		case "up":
-			MovmentUp(direction);
-			break;
-		case "right":
-			MovmentRight(direction);
-			break;
-		case "down":
-			MovmentDown(direction);
-			break;
-		default:
-			break;
-	}
-}
-function getposition() {
-	return document.getElementById(`x${position_x}_y${position_y}`);
-}
+var a = new snake()
 document.addEventListener("keydown", (event) => {
-	console.log("keydown");
 	if (event.isComposing || event.keyCode === 87) {
-		direction = "up";
+		a.Changedirection("up")
 		return;
 	}
 	if (event.isComposing || event.keyCode === 68) {
-		direction = "right";
+		a.Changedirection("right")
 		return;
 	}
 	if (event.isComposing || event.keyCode === 83) {
-		direction = "down";
+		a.Changedirection("down")
 		return;
 	}
 	if (event.isComposing || event.keyCode === 65) {
-		direction = "left";
+		a.Changedirection("left")
 		return;
 	}
 });
