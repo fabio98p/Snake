ab44f221167883621ff490a0d36c02b3ee098a35
fabio98p <parisifabio98@gmail.com>
2022-03-07 01:10:12 +0100

    sistemato estetica e aggiunto mosse e il pulsante inizia gioco

--- main.js
+++ main.js
@@ -4,35 +4,38 @@ class game {
 	direction = "left";
 	position_x = 4;
 	position_y = 4;
-	griglia = document.getElementById("griglia");
+	grid = document.getElementById("grid");
 	div = document.createElement("li");
     Snake = 0
+    score = 0
+    moves = 0
 	constructor() {
 		this.CreationGame();
+        this.CounterMoves();
         this.Snake = new Snake(this.position_x, this.position_y)
         this.Snake.GetSnakeList()
-		this.StartGame();
 	}
 	CreationGame() {
 		for (let y = 0; y < 12; y++) {
 			for (let x = 0; x < 12; x++) {
-				this.griglia.innerHTML += `<li id="x${x}_y${y}" class="cell cell-x${x} cell-y${y}"></li>`;
+				this.grid.innerHTML += `<li id="x${x}_y${y}" class="cell cell-x${x} cell-y${y}"></li>`;
 			}
 		}
 	}
 	StartGame() {
+        console.log("INIZIO");
 		setInterval(() => {
+            this.CounterMoves()
             this.snakeposition = this.Snake.Movment(this.direction)
             this.ClearGrid()
             this.snakeposition.forEach(snakePiece => {
                 this.SetSnakePiece(snakePiece)
             });
-		}, 500);
+		}, 100);
 	}
 	Changedirection(direction){
 		this.direction = direction
 	}
-	
 	ClearGrid() {
         let allCells = document.getElementsByClassName("cell");
         Array.from(allCells).forEach((cell) => {
@@ -42,9 +45,13 @@ class game {
     SetSnakePiece(snakePiece) {
 		document.getElementById(`x${snakePiece.pos_x}_y${snakePiece.pos_y}`).classList.add("active");
 	}
+    CounterMoves(){
+        document.getElementById("moves").innerHTML = ++this.moves
+    }
 }
 //var b = new Snake()
 var a = new game()
+console.log("gioco creato");
 document.addEventListener("keydown", (event) => {
 	if (event.isComposing || event.keyCode === 87) {
 		a.Changedirection("up")
